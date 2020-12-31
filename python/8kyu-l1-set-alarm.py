1a90a45028327abe62c0f15ffcb8b86bccd785cc
EricFletcher1 <64165327+EverythingIsAwesome2009@users.noreply.github.com>
2020-12-31 12:43:13 -0500

    Added L1: Set Alarm

--- python/8kyu-l1-set-alarm.py
+++ python/8kyu-l1-set-alarm.py
@@ -3,7 +3,7 @@ Name: L1: Set Alarm
 Link: https://www.codewars.com/kata/568dcc3c7f12767a62000038/solutions/r
 """
 
-# Solution 1
+# Solution A
 def set_alarm(employed, vacation):
     # If logical test is met, return True.
     if employed == True and vacation == False:
@@ -14,9 +14,10 @@ def set_alarm(employed, vacation):
 
 print(set_alarm(True, False))
 
-# Solution 2
+# Solution B
 def set_alarm2(employed, vacation):
     return employed == True and vacation == False
 
 print(set_alarm2(True, True)) 
+
 
