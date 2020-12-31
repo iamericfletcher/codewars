6244e0527b0b5279f6553e8f4b87180a0a76bb61
EricFletcher1 <64165327+EverythingIsAwesome2009@users.noreply.github.com>
2020-12-31 14:53:57 -0500

    Added 8kyu-l1-set-alarm

---
+++ javascript/8kyu-l1-set-alarm.js
@@ -0,0 +1,34 @@
+// Solution 1:
+
+function setAlarm(employed, vacation){
+    // If logic condition is met, return true. 
+    if (employed == true && vacation == false) {
+        return true;
+    // Otherwise, return false. 
+    } else {
+        return false;
+    }
+}
+
+
+// Solution 2:
+
+function setAlarm2(employed, vacation){
+    // Return true if logical condition is met. Otherwise, return false. 
+    return(employed == true && vacation == false)
+}
+
+// Using Quokka to run the code inside of VS Code for solution 1.
+console.log(setAlarm(true, true), "Should be false.");
+console.log(setAlarm(false, true), "Should be false.");
+console.log(setAlarm(true, false), "Should be true.");
+
+console.log(setAlarm(true, true))
+
+
+// Using Quokka to run the code inside of VS Code for solution 2. 
+console.log(setAlarm2(true, true), "Should be false.");
+console.log(setAlarm2(false, true), "Should be false.");
+console.log(setAlarm2(true, false), "Should be true.");
+
+console.log(setAlarm2(false, false))
